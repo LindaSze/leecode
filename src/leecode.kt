cc5b6d60642045a66106e15f266a2a8775c0232a
lynn <silian916@gmail.com>
2020-01-09 01:00:18 +0800

    two_sum v2

--- src/leecode.kt
+++ src/leecode.kt
@@ -1,6 +1,9 @@
 fun main() {
     var value = twoSum_v1(intArrayOf(3, 2, 4), 6)
-    println("twoSum_v1:"+value[0].toString() + "," + value[1])
+    //println("twoSum_v1:"+value[0].toString() + "," + value[1])
+    value = twoSum_v2(intArrayOf(3, 2, 4), 6)
+    println("twoSum_v2:" + value[0].toString() + "," + value[1])
+
 }
 
 /**
@@ -26,19 +29,24 @@ fun twoSum_v1(nums: IntArray, target: Int): IntArray {
 }
 
 
+/**
+ * 执行用时 :240 ms,
+ * 在所有 Kotlin 提交中击败了47.18%的用户
+ * 内存消耗 :36.7 MB, 在所有 Kotlin 提交中击败了47.18%的用户
+ */
 fun twoSum_v2(nums: IntArray, target: Int): IntArray {
-    var value: IntArray = intArrayOf(0, 0)
-    var isFind: Boolean = false
-    for ((index, num1) in nums.withIndex()) {
-        for (j in index + 1 until nums.size) {
-            value = intArrayOf(index, j)
-            if (num1 + nums[j] == target) {
-                isFind = true;
-                break;
-            }
-        }
-        if (isFind)
-            break;
+    var hashmap = hashMapOf<Int, Int>()
+    for ((index, num) in nums.withIndex()) {
+        hashmap.set( num,index)
     }
-    return value
+
+    for ((index, num) in nums.withIndex()) {
+        if (hashmap.containsKey(target - num)) {
+            var index2 = hashmap.get(target - num)
+            if (index2 != index)
+                return intArrayOf(index, index2!!)
+        }
+    }
+
+    return intArrayOf(0, 0)
 }
