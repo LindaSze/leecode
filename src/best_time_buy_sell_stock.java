b487f899467248564eee307af3768b755db6880c
lynn <silian916@gmail.com>
2020-01-16 21:25:07 +0800

    no message

--- src/best_time_buy_sell_stock.java
+++ src/best_time_buy_sell_stock.java
@@ -1,16 +1,33 @@
 public class best_time_buy_sell_stock {
 
     /**
-     * 买卖股票的最佳时机
-     * 爬n阶，每次可爬1或2个台阶，求共多少种组合--斐波那契额函数
-     * 动态规划法
-     */
+     * 买卖股票的最佳时机，求最大利润
+     * 输入: [7,1,5,3,6,4]，输出: 5
+     * 即在第2天买入，第5天卖出获得最大差值5。
+     **/
 
     public static void main(String[] args) {
-//      System.out.println( climb_Stairs_v1(0, 3));
-//        System.out.println( climb_Stairs_v0(5));
-
+        System.out.println(bese_time_buy_sell_stock(new int[]{1,3,7,3,2,15}));
     }
 
+
+    /**
+     * 思路：循环一次，线性遍历保证顺序，分别保存低谷，和最大差值，并返回后者
+     * 另有暴力解法，两次循环，遍历所有差值，选择最大
+     * 1ms,99.99%,37.5,74%
+     */
+    public static int bese_time_buy_sell_stock(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0, size = prices.length; i < size; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+
+            }
+        }
+        return maxProfit;
+    }
 
 }
---
+++ src/single_number.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class single_number {
+    public static void main(String[] args) {
+        System.out.println(single_number_v2(new int[]{1, 2, 3, 4, 1, 6, 3, 4, 2}));
+
+        System.out.println(single_number_v3(new Integer[]{1, 2, 3, 4, 1, 6, 3, 4, 2}));
+
+    }
+
+    /**
+     * 思路：比暴力解法好一点，采用栈匹配并弹出的方式
+     * 暴力解法是两次循环，遍历数组+已遍历内容是否含有数字
+     * 114,7.21%,40.2,78.76%
+     */
+    public static int single_number_v1(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0, size = nums.length; i < size; i++) {
+            if (stack.contains(nums[i]))
+                stack.remove((Integer) nums[i]);
+            else stack.add(nums[i]);
+        }
+        return stack.get(0);
+    }
+
+    /**
+     * 思路：同上，但数据结构采用hashmap
+     * 同样的匹配，hashmap比栈和linkedlist等结构好
+     * 因为查找匹配数组不需要循环，减少O(n),链表结构式插入方便但查找费力
+     * 11ms，26.92%,39.9,86.8%
+     */
+    public static int single_number_v2(int[] nums) {
+        HashMap<Integer, Integer> stack = new HashMap<>();
+        for (int i = 0, size = nums.length; i < size; i++) {
+            if (stack.containsKey(nums[i]))
+                stack.remove((Integer) nums[i]);
+            else stack.put(nums[i], nums[i]);
+        }
+
+        for (Integer key : stack.keySet()) {
+            return stack.get(key);
+        }
+
+        return 0;
+    }
+
+    /**
+     * 思路：数学-2∗(a+b+c)−(a+a+b+b+c)=c
+     * python比较好实现，java需要collection api
+     * 以及两次遍历，一次求和，一次乘法
+     * 8ms,30.63%,38.3,96.7%
+     */
+    public static int single_number_v3(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        int sum = 0;
+        for (int i = 0, size = nums.length; i < size; i++) {
+            sum += nums[i];
+            set.add(nums[i]);
+        }
+
+        int sum2 = 0;
+        for (Integer num : set) {
+            sum2 += num;
+        }
+        return 2 * sum2 - sum;
+    }
+}
