4fbb9b5e6eda1d39d7c22d66cb3bc885ad55dfe9
司莲 <silian@wintopgroup.com.cn>
2020-02-08 00:34:31 +0800

    no message

--- src/intersection_of_two_linked_ists.java
+++
@@ -1,6 +0,0 @@
-public class intersection_of_two_linked_ists {
-
-    public static void main(String[] args) {
-
-    }
-}
--- src/majority_element.java
+++ src/majority_element.java
@@ -1,22 +1,45 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
  * 多数元素
  * 找到数组中的多数元素，即出现次数大于n/2次的元素
+ * 注意；必须>n/2,就是设定数组中必须有众数，否则会出错
  */
 public class majority_element {
     public static void main(String[] args) {
 
+        int[] a = new int[]{1, 6, 5, 3, 5, 6, 5, 6, 7, 6, 7, 5, 6};
+
+        System.out.println(majority_element_v0(a));
+        System.out.println(majority_element_v2(a));
+
+
+        //System.out.println(majority_element_v4(a, 0, a.length - 1));
     }
 
 
     /**
-     * 逐个遍历匹配，借用hashmap降低维度，在线性时间内遍历nums
+     * 思路：基础解法，逐个遍历匹配，借用hashmap降低维度，在线性时间内使用entrySet遍历nums
+     * 20ms,32%,41,58%
      */
-    public int majority_element_v1(int nums[]){
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+    public static int majority_element_v1(int nums[]) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else map.put(num, 1);
+        }
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+        return majorityEntry.getKey();
     }
 
 
@@ -26,8 +49,70 @@ public class majority_element {
      * 当然偶数情况时，则会是nums.length/2+1也是该元素
      * 1,100%,42.7,6%
      */
-    public int majority_element_v2(int nums[]){
+    public static int majority_element_v2(int nums[]) {
         Arrays.sort(nums);
-        return nums[nums.length/2];
+        return nums[nums.length / 2];
     }
+
+    /**
+     * 思路： Boyer-Moore投票法，计算器
+     * 循环查询，是众数+1，非众数-1，计数器为0则更换众数，因出现次数肯定>n/2，所以循环完毕会得到最后结果
+     * 1ms,100%,42,5%
+     */
+    private static int majority_element_v0(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        int count = 0;
+        int mNum = nums[0];
+        for (int i = 0, size = nums.length; i < size; i++) {
+            if (count == 0) {
+                mNum = nums[i];
+            }
+            if (mNum == nums[i]) {
+                count++;
+            } else
+                count--;
+        }
+        return mNum;
+
+
+        //优化写法，但因为+1一行代码效率变为2ms
+//        for (int num : nums) {
+//            if (count == 0) {
+//                mNum = num;
+//            }
+//            count += (num == mNum) ? 1 : -1;
+//        }
+    }
+
+    private static int countInRange(int[] nums, int num, int lo, int hi) {
+        int count = 0;
+        for (int i = lo; i <= hi; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    private static int majority_element_v4(int[] nums, int lo, int hi) {
+        if (lo == hi) {
+            return nums[lo];
+        }
+
+        int mid = (hi - lo) / 2 + lo;
+        int left = majority_element_v4(nums, lo, mid);
+        int right = majority_element_v4(nums, mid + 1, hi);
+
+        if (left == right) {
+            return left;
+        }
+
+        int leftCount = countInRange(nums, left, lo, hi);
+        int rightCount = countInRange(nums, right, lo, hi);
+
+        return leftCount > rightCount ? left : right;
+    }
+
 }
